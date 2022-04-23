b70960c8e4297d0f4126e76b18e1429bb8c9d1a9
SkrypnichenkoI <plat.iren@gmail.com>
2022-04-23 20:43:52 +0300

    HW02 It seems good variant

--- hw02-gradle/src/main/java/homework/CustomerService.java
+++ hw02-gradle/src/main/java/homework/CustomerService.java
@@ -2,26 +2,42 @@ package homework;
 
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class CustomerService {
-    private TreeMap<Customer, String> customerServiceMap = new TreeMap<>(Comparator.comparingLong(c -> c.getScores()));
+    private final TreeMap<Customer, String> customerServiceMap = new TreeMap<>(Comparator.comparingLong(c -> c.getScores()));
     //todo: 3. надо реализовать методы этого класса
     //важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
 
     public Map.Entry<Customer, String> getSmallest() {
         //Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
-        return customerServiceMap.firstEntry();
+        TreeMap<Customer, String> customerServiceMapCopy = new TreeMap<>(Comparator.comparingLong(c -> c.getScores()));
+        for (Customer key : customerServiceMap.keySet()) {
+            Customer customerCopy = new Customer(key.getId(), key.getName(), key.getScores());
+            String val = new String();
+            val = customerServiceMap.get(key);
+            customerServiceMapCopy.put(customerCopy, val);
+        }
+
+        return customerServiceMapCopy.firstEntry();
 
     }
 
     public Map.Entry<Customer, String> getNext(Customer customer) {
-        return customerServiceMap.ceilingEntry(customer);
+        TreeMap<Customer, String> customerServiceMapCopy = new TreeMap<>(Comparator.comparingLong(c -> c.getScores()));
+        for (Customer key : customerServiceMap.keySet()) {
+            Customer customerCopy = new Customer(key.getId(), key.getName(), key.getScores());
+            String val = new String();
+            val = customerServiceMap.get(key);
+            customerServiceMapCopy.put(customerCopy, val);
+        }
+
+        return customerServiceMapCopy.higherEntry(customer);
     }
 
     public void add(Customer customer, String data) {
-        customerServiceMap.put(customer, data);
-
+        customerServiceMap.put(customer,data);
     }
 }
