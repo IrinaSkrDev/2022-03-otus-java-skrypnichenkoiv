59131fd9bf447b8b6d9e0b0eb23e6522bd4c108e
SkrypnichenkoI <plat.iren@gmail.com>
2022-08-04 22:58:30 +0300

    hw8

--- hw8-json/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ hw8-json/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -4,16 +4,23 @@ import ru.otus.model.Measurement;
 
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
-import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingDouble;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
         //группирует выходящий список по name, при этом суммирует поля value
+
         Map<String, Double> result = data.stream()
-                .collect(groupingBy(Measurement::getName, summingDouble(Measurement::getValue)));
-        return result;
+                .collect(groupingBy((Measurement::getName), summingDouble(Measurement::getValue)));
+        Map<String, Double> resulrCopy = new TreeMap<>();
+        for (String key : result.keySet()) {
+            resulrCopy.put(key, result.get(key));
+        }
+        return resulrCopy;
     }
 }
--- hw8-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
+++ hw8-json/src/main/java/ru/otus/dataprocessor/ResourcesFileLoader.java
@@ -1,31 +1,38 @@
 package ru.otus.dataprocessor;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import ru.otus.model.Measurement;
 
-import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
 public class ResourcesFileLoader implements Loader {
-    File file;
+    String fileName;
     private final ObjectMapper mapper = new ObjectMapper();
 
     public ResourcesFileLoader(String fileName) {
-        this.file = new File(fileName);
+        this.fileName = fileName;
     }
 
     @Override
     public List<Measurement> load() throws IOException {
         //читает файл, парсит и возвращает результат
-        var result = mapper.readValue(this.file, new TypeReference<List<Measurement>>() {
-        });
+        Gson gson = new Gson();
+        var bufferedReader = new BufferedReader(new FileReader(this.fileName));
+        String jsonString = bufferedReader.readLine();
+        Measurement[] result = gson.fromJson(jsonString, Measurement[].class);
+        // Type listType = new TypeToken<ArrayList<Measurement>>(){}.getType();
+        //  List<Measurement> result = new Gson().fromJson(String.valueOf(this.file), listType);
+        // var result = mapper.readValue(this.file, new TypeReference<List<Measurement>>() {
+        // });
         //  mapper.configure(DeserializationFeature.USE_JAVA_ARRAY_FOR_JSON_ARRAY, true);
         //Measurement[] arrayMensurement = mapper.readValue(this.file, Measurement[].class);
         // CollectionType javaType = mapper.getTypeFactory()
         //         .constructCollectionType(List.class, Measurement.class);
         // List<Measurement> result = mapper.readValue(this.file, javaType);
-        return result;
+        return List.of(result);
     }
 }
