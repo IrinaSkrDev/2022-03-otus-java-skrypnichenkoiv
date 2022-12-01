192985156440abd99752ffe91e738d2dfb696e87
SkrypnichenkoI <plat.iren@gmail.com>
2022-12-01 23:28:34 +0300

    HW10 Вопросы

---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,74 @@
+package ru.otus.crm.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "client")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client implements Cloneable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "address_id")
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    private Address address;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "client")
+    private List<Phone> phones;
+
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public Client clone() {
+        return new Client(this.id, this.name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ hw10-jpql/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,33 @@
+package ru.otus.crm.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Phone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "phone_id")
+    private Long phoneId;
+    @Column(name = "phone_number")
+    private String number;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "client_id")
+    @LazyCollection(LazyCollectionOption.EXTRA)
+    private Client client;
+
+    public Phone(Long phoneId, String number) {
+        this.phoneId = phoneId;
+        this.number = number;
+    }
+
+}
