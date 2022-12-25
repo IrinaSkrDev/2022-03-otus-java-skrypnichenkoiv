aeaa48919946efd73c5674927451e4d505cf9f38
SkrypnichenkoI <plat.iren@gmail.com>
2022-12-25 15:02:09 +0300

    hw11

--- hw11-cash/src/main/java/ru/otus/cache/ConsumerClients.java
+++ hw11-cash/src/main/java/ru/otus/cache/ConsumerClients.java
@@ -6,6 +6,6 @@ public class ConsumerClients implements HwListener<Long, Client> {
 
     @Override
     public void notify(Long key, Client value, String action) {
-        System.out.println("Добавлен клиент" + value);
+        System.out.println("New Client has been " + action + "!!!!!!!" + value);
     }
 }
